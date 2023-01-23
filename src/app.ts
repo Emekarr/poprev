0541856d564954d2dc93aa87f7ed302f4cd419be
emekarr <aceanyanwu02@gmail.com>
2023-01-23 14:18:07 +0100

    added ping route and 404 handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
+
+// utils
+import ServerResponse from "./utils/response";
 
 class App {
   express: Application;
@@ -20,6 +23,28 @@ class App {
     this.express.use(
       express.urlencoded({ extended: true, limit: process.env.JSON_LIMIT })
     );
+
+    this.express.use(
+        "/ping",
+        (req: Request, res: Response, next: NextFunction) => {
+          try {
+            new ServerResponse(`messi is the GOAT`).respond(res, 200);
+          } catch (err) {
+            next(err);
+          }
+        }
+      );
+  
+      this.express.use("*", (req: Request, res: Response, next: NextFunction) => {
+        try {
+          new ServerResponse(
+            `the route ${req.method} ${req.baseUrl} does not exist`
+          ).respond(res, 404);
+        } catch (err) {
+          next(err);
+        }
+      });
+  
   }
 
   listen(port: string, cb: () => void) {
