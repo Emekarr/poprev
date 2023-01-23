a7f8fcd469c92e645821bd34c1ed5cf8bcbdc0fe
emekarr <aceanyanwu02@gmail.com>
2023-01-23 14:34:49 +0100

    added CustomError interface for Application and Client errors

--- src/errors/ApplicationError.ts
+++ src/errors/ApplicationError.ts
@@ -1,4 +1,6 @@
-export default class ApplicationError extends Error {
+import { CustomError } from "./types";
+
+export default class ApplicationError extends Error implements CustomError  {
   readonly name = "ApplicationError";
   constructor(message: string, readonly errorCode: number) {
     super(message);
--- src/errors/ClientError.ts
+++ src/errors/ClientError.ts
@@ -1,4 +1,6 @@
-export default class ClientError extends Error {
+import { CustomError } from "./types";
+
+export default class ClientError extends Error implements CustomError {
   readonly name = "ClientError";
   constructor(message: string, readonly errorCode: number) {
     super(message);
