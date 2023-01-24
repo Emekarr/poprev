d8c307b96305785f9d002e861de326d9c81b2eb3
emekarr <aceanyanwu02@gmail.com>
2023-01-25 00:25:54 +0100

    refactor - hash otp before saving

--- src/security/otp.ts
+++ src/security/otp.ts
@@ -1,8 +1,13 @@
 import redisRepo from "../repository/redis";
+import hasher from "./hasher";
 
 export const generateOtp = async (key: string): Promise<string> => {
   const otp = Math.floor(100000 + Math.random() * 899999).toString();
   // cache for 10 mins
-  await redisRepo.createEntryAndExpire(key, otp, 600);
+  await redisRepo.createEntryAndExpire(
+    key,
+    await hasher.hashPassword(otp),
+    600
+  );
   return otp;
 };
